1bf02dffbd139b6a44679ee9a257d3e869c940ba
Francis Gonzales <fgonzalestello91@gmail.com>
2015-10-21 21:30:17 -0500

    changes

--- Cat Years/ViewController.swift
+++ Cat Years/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var catYear: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func findAge(sender: AnyObject) {
+        
+        var catAge = Int(catYear.text!)!
+        
+        catAge = catAge * 7
+        
+        resultLabel.text = "Your cat age is " + String(catAge) + " in cat years"
+    }
 
 }
 
